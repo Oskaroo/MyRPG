c04b01f84ad3261eaa1da5588aa04d69a9e6286f
Oskaroo <oskarmno@gmail.com>
2023-09-06 22:49:38 +0200

    Add database

--- MyApiRPG/Program.cs
+++ MyApiRPG/Program.cs
@@ -2,10 +2,14 @@ global using MyApiRPG.Dtos.Character;
 global using MyApiRPG.Models;
 global using MyApiRPG.Services;
 global using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MyApiRPG.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
