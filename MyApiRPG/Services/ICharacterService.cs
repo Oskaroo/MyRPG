a73502b6a4c401172131b480d22cd4c70c236355
Oskaroo <oskarmno@gmail.com>
2023-09-06 21:11:22 +0200

    Add UpdateCharacter, and exceptions

--- MyApiRPG/Services/ICharacterService.cs
+++ MyApiRPG/Services/ICharacterService.cs
@@ -7,4 +7,8 @@ public interface ICharacterService
     Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
     Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
     Task<ServiceResponse<List<GetCharacterDto>>>AddCharacter(AddCharacterDto newCharacter);
+    Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto newCharacter);
+
+    
+    
 }
