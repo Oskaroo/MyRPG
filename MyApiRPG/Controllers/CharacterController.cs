19a1aa39162a7b32167d2ca063bd655952c8da26
Oskaroo <oskarmno@gmail.com>
2023-09-06 21:18:36 +0200

    Add Delete func

--- MyApiRPG/Controllers/CharacterController.cs
+++ MyApiRPG/Controllers/CharacterController.cs
@@ -38,4 +38,13 @@ public class CharacterController : ControllerBase
             return NotFound(response);
         return Ok(response);
     }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> DeleteCharacter(int id)
+    {
+        var response = await _characterService.DeleteCharacter(id);
+        if (response.Data is null)
+            return NotFound(response);
+        return Ok(response);
+    }
+    
 }
--- MyApiRPG/Services/ICharacterService.cs
+++ MyApiRPG/Services/ICharacterService.cs
@@ -8,6 +8,7 @@ public interface ICharacterService
     Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
     Task<ServiceResponse<List<GetCharacterDto>>>AddCharacter(AddCharacterDto newCharacter);
     Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto newCharacter);
+    Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
 
     
     
